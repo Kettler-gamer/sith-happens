eb6aaf8c03d6887ab1bd68baaab59c74eefdecab
Felix Lindh <felix.lindh@student.kyh.se>
2023-01-25 11:57:12 +0100

    Added card component and adjusted content page render

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
     setCurrentData(data);
   }
 
+
   return (
     <div>
       <Header />
--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,9 +1,12 @@
+import Card from "./Card";
+
 export default function ContentPage(props) {
+  console.log(props)
+  const person = props.data !== undefined ? props.data.results.map((element) => <Card info={element} />) : <p></p>
+  
   return (
     <div>
-      {props.data !== undefined && (
-        <h1>{props.data.results.map((element) => element.name)}</h1>
-      )}
+      {person}
     </div>
   );
 }
