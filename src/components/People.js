6913e115bcf8b69992721f682c3b4053d2c739c5
Felix Lindh <felix.lindh@student.kyh.se>
2023-01-26 12:11:43 +0100

    added fetch for vehicles to People

--- src/components/People.js
+++ src/components/People.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 export default function People(props) {
   const [starShips, setStarships] = useState(undefined);
+  const [vehicles, setVehicles] = useState(undefined);
   const mounted = useRef(false);
 
   function getMovieTitle(id) {
@@ -23,24 +24,25 @@ export default function People(props) {
     }
   }
 
-  async function getStarshipsName(link) {
+  async function getNameFromLink(link) {
     const response = await fetch(link);
     const data = await response.json();
     return data.name;
   }
 
-  async function loadStarships() {
+  async function getNamesFromList(array, callback) {
     const arr = [];
-    for (let link of props.info.starships) {
-      arr.push(await getStarshipsName(link));
+    for (let link of array) {
+      arr.push(await getNameFromLink(link));
     }
-    setStarships(arr);
+    callback(arr);
   }
-
+  //eslint-disable-next-line
   useEffect(() => {
     if (!mounted.current) {
       mounted.current = true;
-      loadStarships();
+      getNamesFromList(props.info.starships, setStarships);
+      getNamesFromList(props.info.vehicles, setVehicles);
     }
   });
 
@@ -75,6 +77,15 @@ export default function People(props) {
       {props.info.starships !== undefined && starShips !== undefined && (
         <ul>
           {starShips.map((element, index) => (
+            <li key={element + index}>{element}</li>
+          ))}
+        </ul>
+      )}
+      {props.info.vehicles !== undefined &&
+        props.info.vehicles.length > 0 && <p>vehicles:</p>}
+      {props.info.vehicles !== undefined && vehicles !== undefined && (
+        <ul>
+          {vehicles.map((element, index) => (
             <li key={element + index}>{element}</li>
           ))}
         </ul>
